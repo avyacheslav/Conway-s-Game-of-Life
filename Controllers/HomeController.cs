d7d151630642b1819866e7206c1320ca214648ba
vargudiaev <vyacheslav.argudiaev@dxc.com>
2022-06-23 04:08:01 +0300

    Add libs.
    obj has been removed. Add js libs.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ConwaysGameOfLife.Processing;
+
+namespace ConwaysGameOfLife.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult GetRandomData()
+        { 
+            return new JsonResult(GameProcessing.GetRandomData());
+        }
+        [HttpPost]
+        public JsonResult NextStep()
+        {
+            if (Request.ContentLength == 0) return null;
+            var requestData = Request.Form.ToList();
+            GameProcessing procecessing = new GameProcessing(requestData);
+            return procecessing.GetNextStep();
+        }
+    }
+}
